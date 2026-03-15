14964611856550ea053131de410ea0fd3376e408
TTB003 <helloamittb@gmail.com>
2026-03-15 11:29:48 +0700

    oke

---
+++ Models/FoodPlace.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodStreetGuide.Models
+{
+    internal class FoodPlace
+    {
+    }
+}
---
+++ Services/SpeechService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodStreetGuide.Services
+{
+    internal class SpeechService
+    {
+    }
+}
